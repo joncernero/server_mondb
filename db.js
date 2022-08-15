e733583f783b6d97c6fe7575010fc204a05b26f1
Jonathan Cernero <cernero.jon@gmail.com>
2022-08-15 09:43:30 -0400

    reverting db back for updated deployment

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('monster-crm-database', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-// const db = new Sequelize(process.env.DATABASE_URL, {
+// const db = new Sequelize('monster-crm-database', 'postgres', 'password', {
+//   host: 'localhost',
 //   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false, // very important
-//     },
-//   },
 // });
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important
+    },
+  },
+});
 
 db.authenticate().then(
   function () {
