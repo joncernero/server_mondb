c98137142b3620ab9a9a317fe98f30c8ce9da799
Jonathan Cernero <cernero.jon@gmail.com>
2024-07-21 18:56:12 -0400

    come on

--- db.js
+++ db.js
@@ -1,21 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(
-  process.env.PGDATABASE,
-  process.env.PGUSER,
-  process.env.PGPASSWORD,
-  {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // very important
-      },
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important
     },
-  }
-);
+  },
+});
+
 db.authenticate()
   .then(() => {
     console.log('Connected to the database successfully.');
@@ -40,3 +35,20 @@ module.exports = db;
 //     },
 //   },
 // });
+
+// const db = new Sequelize(
+//   process.env.PGDATABASE,
+//   process.env.PGUSER,
+//   process.env.PGPASSWORD,
+//   {
+//     host: process.env.PGHOST,
+//     port: process.env.PGPORT,
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false, // very important
+//       },
+//     },
+//   }
+// );
