8d82983ec5a3364199c4b0c70ac265cdd4e9590a
Jonathan Cernero <cernero.jon@gmail.com>
2024-07-21 18:31:25 -0400

    again

--- db.js
+++ db.js
@@ -1,15 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // very important
+const db = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // very important
+      },
     },
-  },
-});
-
+  }
+);
 db.authenticate()
   .then(() => {
     console.log('Connected to the database successfully.');
@@ -23,4 +29,14 @@ module.exports = db;
 // const db = new Sequelize('monster-crm-database', 'postgres', 'password', {
 //   host: 'localhost',
 //   dialect: 'postgres',
+// });
+
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false, // very important
+//     },
+//   },
 // });
